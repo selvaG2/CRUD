db981e51d2d553094536ae0788bbfd94247e20b5
selvaG2 <selvaganapathikg21@gmail.com>
2023-04-29 16:23:16 +0530

    read completed.

---
+++ assets/js/api.js
@@ -0,0 +1,236 @@
+function loadTable() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "http://localhost:3000/company");
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      var trHTML = "";
+      const objects = JSON.parse(this.responseText);
+      for (let object of objects) {
+        trHTML += "<tr>";
+        trHTML += "<td>" + object["id"] + "</td>";
+        trHTML +=
+          '<td><img width="50px" src="' +
+          object["logo"] +
+          '" class="logo"></td>';
+        trHTML += "<td>" + object["company_name"] + "</td>";
+        trHTML += "<td>" + object["type"] + "</td>";
+        trHTML += "<td>" + object["address"] + "</td>";
+        trHTML += "<td>" + object["contact"] + "</td>";
+        trHTML += "<td>" + object["email"] + "</td>";
+        trHTML +=
+          '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox(' +
+          object["id"] +
+          ')">Edit</button>';
+        trHTML +=
+          '<button type="button" class="btn btn-outline-danger" onclick="userDelete(' +
+          object["id"] +
+          ')">Del</button></td>';
+        trHTML += "</tr>";
+      }
+      document.getElementById("mytable").innerHTML = trHTML;
+    }
+  };
+}
+
+loadTable();
+
+function showUserCreateBox() {
+  Swal.fire({
+    title: "Add New Company",
+    html:
+      '<div class="swal2-row">' +
+      '<label for="company_name">Name</label>' +
+      '<input id="company_name" class="swal2-input">' +
+      "</div>" +
+      '<div class="swal2-row">' +
+      '<label for="type">Type</label>' +
+      '<select id="type" class="swal2-input">' +
+      '<option value="IT-Sector" selected>IT-Sector</option>' +
+      '<option value="Finance">Finance</option>' +
+      '<option value="Manufacturing">Manufacturing</option>' +
+      "</select>" +
+      "</div>" +
+      '<div class="swal2-row">' +
+      '<label for="address">Address</label>' +
+      '<input id="address" class="swal2-input">' +
+      "</div>" +
+      '<div class="swal2-row">' +
+      '<label for="contact">Contact</label>' +
+      '<input id="contact" class="swal2-input">' +
+      "</div>" +
+      '<div class="swal2-row">' +
+      '<label for="email">Email</label>' +
+      '<input id="email" class="swal2-input">' +
+      "</div>" +
+      '<div class="swal2-row">' +
+      '<label for="logo">Logo</label>' +
+      '<input type="file" id="logo" class="swal2-input w-25">' +
+      "</div>",
+    focusConfirm: false,
+    showCancelButton: true,
+    preConfirm: () => {
+      userCreate();
+    },
+  });
+}
+
+function userCreate() {
+  const company_name = document.getElementById("company_name").value;
+  const type = document.getElementById("type").value;
+  const address = document.getElementById("address").value;
+  const contact = document.getElementById("contact").value;
+  const email = document.getElementById("email").value;
+  const logo = document.getElementById("logo").files[0];
+
+  console.log(id);
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "http://localhost:3000/company");
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(
+    JSON.stringify({
+      company_name: company_name,
+      type: type,
+      address: address,
+      contact: contact,
+      email: email,
+      logo: logo,
+    })
+  );
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const objects = JSON.parse(this.responseText);
+      Swal.fire(objects["message"]);
+      loadTable();
+    }
+  };
+}
+
+function showUserEditBox(id) {
+  console.log(id);
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", `http://localhost:3000/company/${id}`);
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const objects = JSON.parse(this.responseText);
+      //const user = objects["objects"];
+      console.log(objects);
+      Swal.fire({
+        title: "Add New Company",
+        html:
+          '<div class="swal2-row">' +
+          '<label for="company_name">Name</label>' +
+          '<input id="company_name" class="swal2-input" value="' +
+          objects["company_name"] +
+          '">' +
+          "</div>" +
+          '<div class="swal2-row">' +
+          '<label for="type">Type</label>' +
+          '<select id="type" class="swal2-input" value="' +
+          objects["type"] +
+          '">' +
+          '<option value="IT-Sector" selected>IT-Sector</option>' +
+          '<option value="Finance">Finance</option>' +
+          '<option value="Manufacturing">Manufacturing</option>' +
+          "</select>" +
+          "</div>" +
+          '<div class="swal2-row">' +
+          '<label for="address">Address</label>' +
+          '<input id="address" class="swal2-input" value="' +
+          objects["address"] +
+          '">' +
+          "</div>" +
+          '<div class="swal2-row">' +
+          '<label for="contact">Contact</label>' +
+          '<input id="contact" class="swal2-input" value="' +
+          objects["contact"] +
+          '">' +
+          "</div>" +
+          '<div class="swal2-row">' +
+          '<label for="email">Email</label>' +
+          '<input id="email" class="swal2-input" value="' +
+          objects["email"] +
+          '">' +
+          "</div>" +
+          '<div class="swal2-row">' +
+          '<label for="logo">Logo</label>' +
+          '<input type="file" id="logo" class="swal2-input w-25" value="' +
+          objects["logo"] +
+          '">' +
+          "</div>",
+        focusConfirm: false,
+        showCancelButton: true,
+        preConfirm: () => {
+          userEdit(id);
+        },
+      });
+    }
+  };
+}
+
+function userEdit(id) {
+  const company_name = document.getElementById("company_name").value;
+  const type = document.getElementById("type").value;
+  const address = document.getElementById("address").value;
+  const contact = document.getElementById("contact").value;
+  const email = document.getElementById("email").value;
+  const logo = document.getElementById("logo").files[0];
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("PUT", `http://localhost:3000/company/${id}`);
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(
+    JSON.stringify({
+      company_name: company_name,
+      type: type,
+      address: address,
+      contact: contact,
+      email: email,
+      logo: logo,
+    })
+  );
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const objects = JSON.parse(this.responseText);
+      Swal.fire(objects["message"]);
+      loadTable();
+    }
+  };
+}
+
+
+
+
+
+
+function userDelete(id) {
+  console.log(id);
+  const xhttp = new XMLHttpRequest();
+  xhttp.open(`DELETE`, `http://localhost:3000/company/${id}`);
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    type: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.value) {
+      xhttp.send(JSON.stringify({ id: id }));
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          const objects = JSON.parse(this.responseText);
+          Swal.fire(objects["message"]);
+          loadTable();
+        }
+      };
+    }
+  });
+}
+

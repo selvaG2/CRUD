2921661daa0c4ffc51a6a1cc17edd9d142622e6c
selvaG2 <selvaganapathikg21@gmail.com>
2023-05-03 06:32:37 +0530

    71%

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,5 @@
+//Load table function
+
 function loadTable() {
   const xhttp = new XMLHttpRequest();
   xhttp.open("GET", "http://localhost:3000/company");
@@ -34,7 +36,10 @@ function loadTable() {
   };
 }
 
-loadTable();
+loadTable(); //automatically invoke once the page is loaded/reloaded
+
+
+//User search function with company name
 
 function userSearch(company_name) {
   const xhttp = new XMLHttpRequest();
@@ -79,6 +84,9 @@ function userSearch(company_name) {
   xhttp.send();
 }
 
+
+//User create box 
+
 function showUserCreateBox() {
   Swal.fire({
     title: "Add New Company",
@@ -118,6 +126,8 @@ function showUserCreateBox() {
     },
   });
 }
+
+//User create function
 
 function userCreate() {
   const company_name = document.getElementById("company_name").value;
@@ -171,6 +181,7 @@ function userCreate() {
   fileXhttp.send(formData);
 }
 
+//User edit box
 
 function showUserEditBox(id) {
   console.log(id);
@@ -234,6 +245,8 @@ function showUserEditBox(id) {
     }
   };
 }
+
+//useredit function
 
 function userEdit(id) {
   const company_name = document.getElementById("company_name").value;
@@ -304,39 +317,44 @@ function userEdit(id) {
   });
 }
 
+//User delete function
 
 function userDelete(id) {
   console.log(id);
-  const xhttp = new XMLHttpRequest();
-  xhttp.open(`DELETE`, `http://localhost:3000/company/${id}`);
-  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
   Swal.fire({
     title: "Are you sure?",
     text: "You won't be able to revert this!",
-    type: "warning",
+    icon: "warning",
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
     confirmButtonText: "Yes, delete it!",
   }).then((result) => {
     if (result.value) {
-      xhttp.send(JSON.stringify({ id: id }));
+      const xhttp = new XMLHttpRequest();
+      xhttp.open(`DELETE`, `http://localhost:3000/company/${id}`);
+      xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhttp.send(
+        JSON.stringify({
+          id: id,
+        })
+      );
       xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4) {
+          const objects = JSON.parse(this.responseText);
           Swal.fire({
+            title: "Deleted!",
+            text: "The user has been deleted.",
             icon: "success",
-            title: "Deleted sucessfully..!",
-            showConfirmButton: true,
+            timer: 9000, // Display the message for 3 seconds
+            timerProgressBar: true, // Show the progress bar
+            didOpen: () => {
+              Swal.showLoading(); // Show a loading animation until the timer expires
+            },
+            willClose: () => {
+              loadTable(); // Reload the table after deleting the user
+            },
           });
-          loadTable();
-        } else {
-          Swal.fire({
-            icon: "failure",
-            title: "Opps..! Deletion failed..!",
-            showConfirmButton: true,
-          });
-          loadTable();
         }
       };
     }

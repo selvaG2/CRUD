454b591aaa6ad12aaed99e62c8201e168774c97e
selvaG2 <selvaganapathikg21@gmail.com>
2023-05-04 00:31:46 +0530

    97

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,28 @@
-$(document).ready(function () {
-  $("#inlineFormCheck").click(function () {
-    if ($(this).prop("checked")) {
-      $("#pass").attr("type", "text");
-    } else {
-      $("#pass").attr("type", "password");
-    }
-  });
+$(".toggle-password").click(function () {
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
 });
 
+function validateUser(event) {
+  var Username = document.getElementById("Username").value;
+  var password = document.getElementById("password").value;
+  var errorElement = document.getElementById("error");
 
+  if (Username == "" || password == "") {
+    errorElement.innerHTML = "Fields can't be empty";
+    event.preventDefault();
+    return;
+  }
+
+  if (Username != "admin" || password != "admin@123") {
+    errorElement.innerHTML = "User name or password is incorrect";
+    event.preventDefault();
+    return;
+    }
+
+}
